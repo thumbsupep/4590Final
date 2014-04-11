e667d202224a8e54a5e274ec6905cc73e302b324
Erica Pramer <erica.pramer@gatech.edu>
2014-04-11 04:59:16 -0400

    Added "glass"
    created the google glass visual in the right hand corner. 40% of the
    screen width, 30% of the screen height

---
+++ 4590FinalProject/Assets/scripts/Visuals.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Visuals : MonoBehaviour {
+
+	float width, height;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		width = Screen.width * .4f;
+		height = Screen.height * .3f;
+		GUI.Box(new Rect(Screen.width-width,0,width,height), "");
+	}
+}
