c130f9da289471f2c48fb826c52f1125bbf95951
Erica Pramer <erica.pramer@gatech.edu>
2014-04-18 12:15:22 -0400

    Revert "Merge branch 'master' of https://github.com/thumbsupep/4590Final"
    This reverts commit e00870532d6607411ec26271812ebcd36ef548e5, reversing
    changes made to 6d4e4e8b64e6d1792556894e366335569ebae505.

--- 4590FinalProject/Assets/scripts/friendFinder.cs
+++ 4590FinalProject/Assets/scripts/friendFinder.cs
@@ -5,41 +5,31 @@ public class friendFinder : MonoBehaviour {
 
 
 	Vector3 prevPos,currPos;
-	float initialPitch, ambientVol, lowerVol;
-	int fr;
+	float initialPitch, ambientVol;
 
 	void Start () {
-		fr = 0;
 		initialPitch = audio.pitch;
 		ambientVol = GameObject.FindWithTag("ambient").audio.volume;
-		lowerVol = GameObject.FindWithTag("ambient").audio.volume - 0.5F;
 		currPos = this.transform.position;
+		audio.Play();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		currPos = this.transform.position;
-
-		if(Input.GetKeyDown(KeyCode.R))
+		if(Input.GetKeyDown(KeyCode.E))
 		{
-			if(fr < 2)
-			{
-				fr++;
-				GameObject.FindWithTag("ambient").audio.volume = lowerVol;
-			}
-			else
-			{
-				fr=0;
+			GameObject.FindWithTag("ambient").audio.volume -= 0.15F;
+			audio.mute = !audio.mute;
+			if(audio.mute == true)
 				GameObject.FindWithTag("ambient").audio.volume = ambientVol;
-			}
 		}
 
-		if(fr==1)
-			currPos = GameObject.Find("friend1").transform.position;
-		else
-			currPos = GameObject.Find("friend1").transform.position;
-
-		if((Vector3.Distance (currPos, GameObject.FindWithTag("Player").transform.position)) < 15 )
+		if(Vector3.Distance (currPos, GameObject.FindWithTag("Player").transform.position) < 5 )
+		{
+			audio.pitch = initialPitch + 0.7F;
+		}
+		else if((Vector3.Distance (currPos, GameObject.FindWithTag("Player").transform.position)) < 15 )
 		{
 			audio.pitch = initialPitch + 0.3F;
 		}
@@ -51,40 +41,5 @@ public class friendFinder : MonoBehaviour {
 		{
 			audio.pitch = initialPitch;
 		}
-
-		if(fr==1)
-		{
-			pointAt.target = GameObject.Find("friend1").transform;
-			GameObject.Find("Top").renderer.enabled = true;
-			GameObject.Find("Back").renderer.enabled = true;
-			GameObject.Find("Bottom").renderer.enabled = true;
-			GameObject.Find("Front").renderer.enabled = true;
-			GameObject.Find("Left").renderer.enabled = true;
-			GameObject.Find("Right").renderer.enabled = true;
-			GameObject.Find("friend1").audio.Play();
-		}
-		else if(fr==2)
-		{
-			GameObject.Find("Top").renderer.enabled = true;
-			GameObject.Find("Back").renderer.enabled = true;
-			GameObject.Find("Bottom").renderer.enabled = true;
-			GameObject.Find("Front").renderer.enabled = true;
-			GameObject.Find("Left").renderer.enabled = true;
-			GameObject.Find("Right").renderer.enabled = true;
-			pointAt.target = GameObject.Find("friend2").transform;
-			GameObject.Find("friend2").audio.Play();
-		}
-		else if(fr==0)
-		{
-			GameObject.Find("Top").renderer.enabled = false;
-			GameObject.Find("Back").renderer.enabled = false;
-			GameObject.Find("Bottom").renderer.enabled = false;
-			GameObject.Find("Front").renderer.enabled = false;
-			GameObject.Find("Left").renderer.enabled = false;
-			GameObject.Find("Right").renderer.enabled = false;
-			GameObject.Find("friend1").audio.Stop();
-			GameObject.Find("friend2").audio.Stop();
-		}
-
 	}
 }
