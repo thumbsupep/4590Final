a42905d7ba2a7451336838905605677b0b8f41be
Erica Pramer <erica.pramer@gatech.edu>
2014-04-18 05:17:44 -0400

    friend finder changes
    Still working on getting the script to switch between two friends.
    Visuals are done and 3/4 sounds are in place

--- 4590FinalProject/Assets/scripts/friendFinder.cs
+++ 4590FinalProject/Assets/scripts/friendFinder.cs
@@ -5,25 +5,35 @@ public class friendFinder : MonoBehaviour {
 
 
 	Vector3 prevPos,currPos;
-	float initialPitch, ambientVol;
+	float initialPitch, ambientVol, lowerVol;
+	public static int fr;
 
 	void Start () {
+		fr = 0;
 		initialPitch = audio.pitch;
 		ambientVol = GameObject.FindWithTag("ambient").audio.volume;
+		lowerVol = GameObject.FindWithTag("ambient").audio.volume - 0.5F;
 		currPos = this.transform.position;
-		audio.Play();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		currPos = this.transform.position;
-		if(Input.GetKeyDown(KeyCode.E))
+		if(fr == 1)
 		{
-			GameObject.FindWithTag("ambient").audio.volume -= 0.15F;
-			audio.mute = !audio.mute;
-			if(audio.mute == true)
-				GameObject.FindWithTag("ambient").audio.volume = ambientVol;
+		currPos = this.transform.position;
+
+		if(Input.GetKeyDown(KeyCode.R))
+		{
+				GameObject.FindWithTag("ambient").audio.volume = lowerVol;
+				audio.mute = !audio.mute;
+				if(audio.mute == true)
+					GameObject.FindWithTag("ambient").audio.volume = ambientVol;
 		}
+
+		if(fr==1)
+			currPos = GameObject.Find("friend1").transform.position;
+		else
+			currPos = GameObject.Find("friend2").transform.position;
 
 		if(Vector3.Distance (currPos, GameObject.FindWithTag("Player").transform.position) < 5 )
 		{
@@ -41,5 +51,41 @@ public class friendFinder : MonoBehaviour {
 		{
 			audio.pitch = initialPitch;
 		}
+
+		if(fr==1)
+		{
+			pointAt.target = GameObject.Find("friend1").transform;
+			GameObject.Find("Top").renderer.enabled = true;
+			GameObject.Find("Back").renderer.enabled = true;
+			GameObject.Find("Bottom").renderer.enabled = true;
+			GameObject.Find("Front").renderer.enabled = true;
+			GameObject.Find("Left").renderer.enabled = true;
+			GameObject.Find("Right").renderer.enabled = true;
+			GameObject.Find("friend1").audio.Play();
+		}
+		else if(fr==2)
+		{
+			GameObject.Find("Top").renderer.enabled = true;
+			GameObject.Find("Back").renderer.enabled = true;
+			GameObject.Find("Bottom").renderer.enabled = true;
+			GameObject.Find("Front").renderer.enabled = true;
+			GameObject.Find("Left").renderer.enabled = true;
+			GameObject.Find("Right").renderer.enabled = true;
+			pointAt.target = GameObject.Find("friend2").transform;
+			GameObject.Find("friend2").audio.Play();
+		}
+		else if(fr==0)
+		{
+			GameObject.Find("Top").renderer.enabled = false;
+			GameObject.Find("Back").renderer.enabled = false;
+			GameObject.Find("Bottom").renderer.enabled = false;
+			GameObject.Find("Front").renderer.enabled = false;
+			GameObject.Find("Left").renderer.enabled = false;
+			GameObject.Find("Right").renderer.enabled = false;
+				audio.Stop ();
+		}
+
+		}
+
 	}
 }
